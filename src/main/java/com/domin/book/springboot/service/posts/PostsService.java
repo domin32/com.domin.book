177f67e99e30a5e41c342cae8bc8271f9f7cf208
yangsungmin <yanggun322@gmail.com>
2022-05-24 21:46:04 +0900

    api update

---
+++ src/main/java/com/domin/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.domin.book.springboot.service.posts;
+
+import com.domin.book.springboot.domain.posts.PostsRepository;
+import com.domin.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
