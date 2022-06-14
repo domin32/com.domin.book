4b7ea191cd77d09cddabe063f23e56ef532b5d34
yangsungmin <yanggun322@gmail.com>
2022-06-14 18:21:56 +0900

    Posts List Update

--- src/main/java/com/domin/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/domin/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.domin.book.springboot.service.posts;
 
 import com.domin.book.springboot.domain.posts.Posts;
 import com.domin.book.springboot.domain.posts.PostsRepository;
+import com.domin.book.springboot.web.dto.PostsListResponseDto;
 import com.domin.book.springboot.web.dto.PostsResponseDto;
 import com.domin.book.springboot.web.dto.PostsSaveRequestDto;
 import com.domin.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(("해당 게시글이 없습니다. id="+id)));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
