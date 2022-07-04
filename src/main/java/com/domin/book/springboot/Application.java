3eadea0ddc1c802bb7144c37024643a811ceab62
yangsungmin <yanggun322@gmail.com>
2022-07-04 20:43:15 +0900

    TDD Environmental settings

--- src/main/java/com/domin/book/springboot/Application.java
+++ src/main/java/com/domin/book/springboot/Application.java
@@ -2,8 +2,7 @@ package com.domin.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-@EnableJpaAuditing
+// @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
